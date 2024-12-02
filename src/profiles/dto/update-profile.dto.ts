cdf25261771af255cfb63bfbe30f20790f658bc5
Maksat <106050664+MaxatDzhetenbayev@users.noreply.github.com>
2024-12-02 23:08:36 +0500

    fix: Правка DTO для обновление профиля пользтвателя

--- src/profiles/dto/update-profile.dto.ts
+++ src/profiles/dto/update-profile.dto.ts
@@ -1,4 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateProfileDto } from './create-profile.dto';
+import { Exclude } from 'class-transformer';
 
-export class UpdateProfileDto extends PartialType(CreateProfileDto) {}
+export class UpdateProfileDto extends PartialType(CreateProfileDto) {
+  @Exclude()
+  userId?: number;
+}
